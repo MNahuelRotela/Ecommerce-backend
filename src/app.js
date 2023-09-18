fef1b129ac9aefb30a5826314f48d3eac810f649
MNahuelRotela <mnrqh@live.com.ar>
2023-09-18 08:35:15 -0300

    Final commit backend

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ server.use(cookieParser());
 server.use(morgan('dev'));
 server.use(cors());
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin','http://localhost:5173'); 
+  res.header('Access-Control-Allow-Origin','https://ecommerce-frontend-nine-virid.vercel.app'); 
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
